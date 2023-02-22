021633a883a5b86e9780ec09b3bfd884f5f9f865
Jakub Mitura <jakub.mitura14@gmail.com>
2023-02-22 10:18:15 +0000

    MNIST_VAE works
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- super_voxels/VAE/simple_flax_vae.py
+++ super_voxels/VAE/simple_flax_vae.py
@@ -1,139 +1,211 @@
+# Copyright 2022 The Flax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from absl import app
 from absl import flags
-
+from flax import linen as nn
+from flax.training import train_state
 import jax.numpy as jnp
-import numpy as np
-
 import jax
-from jax import nn as jnn
 from jax import random
-
-from flax import nn
-from flax import optim
-
+import numpy as np
+import optax
+import tensorflow as tf
 import tensorflow_datasets as tfds
 
+import utils as vae_utils
 
-EPS = 1e-8
-KEY = random.PRNGKey(0)
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_float(
     'learning_rate', default=1e-3,
-    help=('The leanring rate for the Adam optimizer')
+    help=('The learning rate for the Adam optimizer.')
 )
 
 flags.DEFINE_integer(
     'batch_size', default=128,
-    help=('Batch size for training')
+    help=('Batch size for training.')
 )
 
 flags.DEFINE_integer(
-    'num_epochs', default=10,
-    help=('Number of training epochs')
+    'num_epochs', default=30,
+    help=('Number of training epochs.')
+)
+
+flags.DEFINE_integer(
+    'latents', default=20,
+    help=('Number of latent variables.')
 )
 
 
 class Encoder(nn.Module):
-    def apply(self, x):
-        x = nn.Dense(x, 400, name='enc_fc1')
-        x = jnn.relu(x)
-        mean_x = nn.Dense(x, 20, name='enc_fc21')
-        logvar_x = nn.Dense(x, 20, name='enc_fc22')
-        return mean_x, logvar_x
+  latents: int
+
+  @nn.compact
+  def __call__(self, x):
+    x = nn.Dense(500, name='fc1')(x)
+    x = nn.relu(x)
+    mean_x = nn.Dense(self.latents, name='fc2_mean')(x)
+    logvar_x = nn.Dense(self.latents, name='fc2_logvar')(x)
+    return mean_x, logvar_x
 
 
 class Decoder(nn.Module):
-    def apply(self, z):
-        z = nn.Dense(z, 400, name='dec_fc1')
-        z = jnn.relu(z)
-        z = nn.Dense(z, 784, name='dec_fc2')
-        z = jnn.sigmoid(z)
-        return z
+
+  @nn.compact
+  def __call__(self, z):
+    z = nn.Dense(500, name='fc1')(z)
+    z = nn.relu(z)
+    z = nn.Dense(784, name='fc2')(z)
+    return z
 
 
 class VAE(nn.Module):
-    def apply(self, x):
-        mean, logvar = Encoder(x, name='encoder')
-        z = reparameterize(mean, logvar)
-        recon_x = Decoder(z, name='decoder')
-        return recon_x, mean, logvar
+  latents: int = 20
 
-    @nn.module_method
-    def generate(self, z):
-        params = self.get_param("decoder")
-        return Decoder.call(params, z)
+  def setup(self):
+    self.encoder = Encoder(self.latents)
+    self.decoder = Decoder()
+
+  def __call__(self, x, z_rng):
+    mean, logvar = self.encoder(x)
+    z = reparameterize(z_rng, mean, logvar)
+    recon_x = self.decoder(z)
+    return recon_x, mean, logvar
+
+  def generate(self, z):
+    return nn.sigmoid(self.decoder(z))
 
 
-def reparameterize(mean, logvar):
-    std = jnp.exp(0.5 * logvar)
-    eps = np.random.normal(size=logvar.shape)
-    return mean + eps * std
+def reparameterize(rng, mean, logvar):
+  std = jnp.exp(0.5 * logvar)
+  eps = random.normal(rng, logvar.shape)
+  return mean + eps * std
 
 
 @jax.vmap
 def kl_divergence(mean, logvar):
-    return - 0.5 * jnp.sum(1 + logvar - jnp.power(mean, 2) - jnp.exp(logvar))
+  return -0.5 * jnp.sum(1 + logvar - jnp.square(mean) - jnp.exp(logvar))
 
 
 @jax.vmap
-def binary_cross_entropy(probs, labels):
-    return - jnp.sum(labels * jnp.log(probs + EPS) + (1 - labels) * jnp.log(1 - probs + EPS))
+def binary_cross_entropy_with_logits(logits, labels):
+  logits = nn.log_sigmoid(logits)
+  return -jnp.sum(labels * logits + (1. - labels) * jnp.log(-jnp.expm1(logits)))
 
 
 def compute_metrics(recon_x, x, mean, logvar):
-    BCE = binary_cross_entropy(recon_x, x)
-    KLD = kl_divergence(mean, logvar)
-    return {'bce': jnp.mean(BCE), 'kld': jnp.mean(KLD), 'loss': jnp.mean(BCE + KLD)}
+  bce_loss = binary_cross_entropy_with_logits(recon_x, x).mean()
+  kld_loss = kl_divergence(mean, logvar).mean()
+  return {
+      'bce': bce_loss,
+      'kld': kld_loss,
+      'loss': bce_loss + kld_loss
+  }
+
+
+def model():
+  return VAE(latents=FLAGS.latents)
 
 
 @jax.jit
-def train_step(optimizer, batch):
-    def loss_fn(model):
-        x = batch['image']
-        recon_x, mean, logvar = model(x)
+def train_step(state, batch, z_rng):
+  def loss_fn(params):
+    recon_x, mean, logvar = model().apply({'params': params}, batch, z_rng)
 
-        BCE = binary_cross_entropy(recon_x, x)
-        KLD = kl_divergence(mean, logvar)
-        loss = jnp.mean(BCE + KLD)
-        return loss, recon_x
-    optimizer, _, _ = optimizer.optimize(loss_fn)
-    return optimizer
+    bce_loss = binary_cross_entropy_with_logits(recon_x, batch).mean()
+    kld_loss = kl_divergence(mean, logvar).mean()
+    loss = bce_loss + kld_loss
+    return loss
+  grads = jax.grad(loss_fn)(state.params)
+  return state.apply_gradients(grads=grads)
 
 
 @jax.jit
-def eval(model, eval_ds, z):
-    xs = eval_ds['image'] / 255.0
-    xs = xs.reshape(-1, 784)
-    recon_xs, mean, logvar = model(xs)
+def eval(params, images, z, z_rng):
+  def eval_model(vae):
+    recon_images, mean, logvar = vae(images, z_rng)
+    comparison = jnp.concatenate([images[:8].reshape(-1, 28, 28, 1),
+                                  recon_images[:8].reshape(-1, 28, 28, 1)])
 
-    comparison = jnp.concatenate([xs[:8].reshape(-1, 28, 28, 1),
-                                  recon_xs[:8].reshape(-1, 28, 28, 1)])
+    generate_images = vae.generate(z)
+    generate_images = generate_images.reshape(-1, 28, 28, 1)
+    metrics = compute_metrics(recon_images, images, mean, logvar)
+    return metrics, comparison, generate_images
 
-    generate_xs = model.generate(z)
-    generate_xs = generate_xs.reshape(-1, 28, 28, 1)
-
-    return compute_metrics(recon_xs, xs, mean, logvar), comparison, generate_xs
+  return nn.apply(eval_model, model())({'params': params})
 
 
-train_ds = tfds.load('mnist', split=tfds.Split.TRAIN)
-train_ds = train_ds.cache().shuffle(1000).batch(FLAGS.batch_size)
-test_ds = tfds.as_numpy(tfds.load('mnist', split=tfds.Split.TEST, batch_size=-1))
+def prepare_image(x):
+  x = tf.cast(x['image'], tf.float32)
+  x = tf.reshape(x, (-1,))
+  return x
 
-_, vae = VAE.create_by_shape(KEY, [((1, 784), jnp.float32)])
 
-optimizer = optim.Adam(learning_rate=FLAGS.learning_rate).create(vae)
+def main(argv):
+  del argv
 
-for epoch in range(FLAGS.num_epochs):
-    for batch in tfds.as_numpy(train_ds):
-        batch['image'] = batch['image'].reshape(-1, 784) / 255.0
-        optimizer = train_step(optimizer, batch)
+  # Make sure tf does not allocate gpu memory.
+  tf.config.experimental.set_visible_devices([], 'GPU')
 
-    z = np.random.normal(size=(64, 20))
-    metrics, comparison, sample = eval(optimizer.target, test_ds, z)
+  rng = random.PRNGKey(0)
+  rng, key = random.split(rng)
 
-    print("eval epoch: {}, loss: {:.4f}, BCE: {:.4f}, KLD: {:.4f}".format(
+  ds_builder = tfds.builder('binarized_mnist')
+  ds_builder.download_and_prepare()
+  train_ds = ds_builder.as_dataset(split=tfds.Split.TRAIN)
+  train_ds = train_ds.map(prepare_image)
+  train_ds = train_ds.cache()
+  train_ds = train_ds.repeat()
+  train_ds = train_ds.shuffle(50000)
+  train_ds = train_ds.batch(FLAGS.batch_size)
+  train_ds = iter(tfds.as_numpy(train_ds))
+
+  test_ds = ds_builder.as_dataset(split=tfds.Split.TEST)
+  test_ds = test_ds.map(prepare_image).batch(10000)
+  test_ds = np.array(list(test_ds)[0])
+  test_ds = jax.device_put(test_ds)
+
+  init_data = jnp.ones((FLAGS.batch_size, 784), jnp.float32)
+
+  state = train_state.TrainState.create(
+      apply_fn=model().apply,
+      params=model().init(key, init_data, rng)['params'],
+      tx=optax.adam(FLAGS.learning_rate),
+  )
+
+  rng, z_key, eval_rng = random.split(rng, 3)
+  z = random.normal(z_key, (64, FLAGS.latents))
+
+  steps_per_epoch = 50000 // FLAGS.batch_size
+
+  for epoch in range(FLAGS.num_epochs):
+    for _ in range(steps_per_epoch):
+      batch = next(train_ds)
+      rng, key = random.split(rng)
+      state = train_step(state, batch, key)
+
+    metrics, comparison, sample = eval(state.params, test_ds, z, eval_rng)
+    # vae_utils.save_image(
+    #     comparison, f'results/reconstruction_{epoch}.png', nrow=8)
+    # vae_utils.save_image(sample, f'results/sample_{epoch}.png', nrow=8)
+
+    print('eval epoch: {}, loss: {:.4f}, BCE: {:.4f}, KLD: {:.4f}'.format(
         epoch + 1, metrics['loss'], metrics['bce'], metrics['kld']
     ))
 
+
+if __name__ == '__main__':
+  app.run(main)
